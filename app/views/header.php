c4f2049a2cc08d4cd4c53b18f99f069504201a21
tay-rel <sirenita0197@gmail.com>
2023-03-17 12:36:26 +0100

    Ejercicio 3: Añadir logout en parte administrador

--- app/views/header.php
+++ app/views/header.php
@@ -82,6 +82,9 @@
                 <li class="nav-item">
                     <a href="<?= ROOT ?>adminProduct" class="nav-link">Productos</a>
                 </li>
+                <li class="nav-item">
+                    <a href="<?= ROOT ?>shop/logout" class="nav-link">Cerrar Sesion</a>
+                </li>
             </ul>
         <?php endif; ?>
     </div>
