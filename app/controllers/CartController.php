8e2d3c77408102cf9042eb57c62e39fbffadb9a8
tay-rel <sirenita0197@gmail.com>
2023-03-22 00:38:52 +0100

    Ejercicio 17:guardar precio

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -307,6 +307,13 @@ class CartController extends Controller
         $user = $session->getUser();
 	 
 			 if ($session->getLogin()) {
+					//variable para obtener el producto del carrito
+					$prices= $this->model->getProductPrices($user->id);
+				 
+					//foreach recorre prices, que tiene la longitud de lo que tenga carrito
+					foreach ($prices as $price){
+						 $this->model->updateProductPrice($price->product_id,$user->id,$price->price);
+					}
 					 if ($this->model->closeCart($user->id, 1)) {
 	 
 							 $data = [
